10873edf120eb873611768207b57b0337e047acc
23235aa <23235aa@hvhs.school.nz>
2026-02-05 10:58:02 +1300

    form submit

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function start() {
     alert("Congratulations!\nYou got the script running.")
 }
+const NAME_FIELD =  document.getElementById("nameField")
+let userName = NAME_FIELD.value;
+
+function getFormInput(){
+    const NAME_FIELD =  document.getElementById("nameField")
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML= "<p> Your Name is "  +userName+"</p>"
+}
